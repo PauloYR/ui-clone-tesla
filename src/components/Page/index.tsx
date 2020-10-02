884498df9f706acc0a2c25b966a598678f64a15c
Paulo Iury <pauloyr@outlook.com>
2020-10-02 17:02:26 -0300

    first commit

---
+++ src/components/Page/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import DefaultOverlayContent from '../DefaultOverlayContent';
+
+import { Container } from './styles';
+
+import { ModelSection, ModelsWrapper } from '../Model';
+
+const Page: React.FC = () => {
+  return (
+    <Container>
+       <ModelsWrapper>
+        <div>
+          {[
+            'Model One',
+            'Model Two',
+            'Model Three',
+            'Model Four',
+            'Model Five',
+            'Model Six',
+            'Model Seven'
+          ].map(modelName => (
+            <ModelSection
+              key={modelName}
+              className="colored"
+              modelName={modelName}
+              overlayNode={
+                <DefaultOverlayContent
+                  label={modelName}
+                  description="Order Online for Delivery"
+                />
+              }
+            />
+          ))}
+        </div>
+
+        {/* <Spacer />
+
+        <UniqueOverlay /> */}
+      </ModelsWrapper>
+    </Container>
+  );
+};
+
+export default Page;
+
